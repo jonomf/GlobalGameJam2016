e40a17742473256e8b6945a170b621a686d428d3
Sam Shapiro <samuel.shapiro123@gmail.com>
2016-01-30 14:52:06 -0500

    added enemy movement animation

--- Assets/Scripts/Mobs/FollowEnemy.cs
+++ Assets/Scripts/Mobs/FollowEnemy.cs
@@ -7,6 +7,16 @@ public class FollowEnemy : MonoBehaviour {
 	public float speed = 5;
 	public float damage = 5;
 
+	Animator anim;
+
+	private Vector3 direction;
+
+	float angle;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
 	void OnCollisionEnter2D(Collision2D other) {
 		if (other.gameObject.layer == Layers.PlayerArrowNum){
 			Destroy(other.gameObject);
@@ -23,6 +33,22 @@ public class FollowEnemy : MonoBehaviour {
 	}
 
 	private void MoveTowardsPlayer() {
-		GetComponent<Rigidbody2D>().MovePosition(transform.position + (Player.Position - transform.position).normalized * speed * Time.deltaTime);
+		direction = (Player.Position - transform.position).normalized;
+		GetComponent<Rigidbody2D>().MovePosition(transform.position + direction * speed * Time.deltaTime);	
+
+		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+		if (angle >= 45 && angle < 135) {
+			anim.Play ("enemyUp");
+		}
+		else if ((angle >= 135 && angle <=180) || (angle <= -135 && angle >= -180)) {
+			anim.Play ("enemyLeft");
+		}
+		else if (angle <= -45 && angle > -135) {
+			anim.Play ("enemyDown");
+		}
+		else {
+			anim.Play ("enemyRight");
+		}
 	}
 }
