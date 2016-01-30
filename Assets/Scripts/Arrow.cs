6258ff851b4b22345652af26508a386e49bddc97
Jono <jono@defectivestudios.com>
2016-01-30 10:14:19 -0500

    Reticle shows up below player; arrows destroy themselves offscreen.

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -3,7 +3,18 @@ using System.Collections;
 
 public class Arrow : MonoBehaviour {
 
+	public float destroyDelay = 2f;
+
+	void Awake() {
+		StartCoroutine(DestroyAfterDelay());
+	}
+
 	void OnCollisionEnter2D(Collision2D coll) {
+		Destroy(gameObject);
+	}
+
+	private IEnumerator DestroyAfterDelay() {
+		yield return new WaitForSeconds(destroyDelay);
 		Destroy(gameObject);
 	}
 }
