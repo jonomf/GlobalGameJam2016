2d72f35664b61acaa5c6ad77d3faeac82f5bf62b
Rohit Crasta <rohitcrasta@gmail.com>
2016-01-30 18:18:22 -0500

    Enemies take multiple hits before dying

--- Assets/Scripts/Mobs/FollowEnemy.cs
+++ Assets/Scripts/Mobs/FollowEnemy.cs
@@ -8,6 +8,9 @@ public class FollowEnemy : MonoBehaviour {
 	public float damage = 10;
 
     public const int FOLLOW_DISTANCE = 8;
+    
+    public const int STARTING_HEALTH = 3;
+    public int Health { get; private set; }
 
 	Animator anim;
 
@@ -15,13 +18,13 @@ public class FollowEnemy : MonoBehaviour {
 
 	void Start () {
 		anim = GetComponent<Animator> ();
+        Health = STARTING_HEALTH;
 	}
 
 	void OnCollisionEnter2D(Collision2D other) {
 		if (other.gameObject.layer == Layers.PlayerArrowNum){
 			Destroy(other.gameObject);
-			Destroy(gameObject);
-            GodManager.updateBars(5,-3,0);
+			GetHurt();
 		}
 		if (other.gameObject.layer == Layers.PlayerNum){
 			Destroy(gameObject);
@@ -52,4 +55,12 @@ public class FollowEnemy : MonoBehaviour {
 			anim.Play("enemyRight");
 		}
 	}
+
+    public void GetHurt(int damage = 1) {
+        Health -= damage;
+        GodManager.updateBars(2,-1,0);
+        if (Health <= 0){
+            Destroy(gameObject);
+        }
+    }
 }
