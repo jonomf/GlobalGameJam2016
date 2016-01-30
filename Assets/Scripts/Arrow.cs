c0afd4544f54cb73f8063e079af2f56922890d19
Sam Shapiro <samuel.shapiro123@gmail.com>
2016-01-29 23:29:44 -0500

    working on player animation

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -1,28 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Arrow : MonoBehaviour {
 
-	public float speed;
+	string direction = "";
+	float speed = 20;
+
+	// Use this for initialization
+	void Start () {
 	
+	}
+	
+	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.UpArrow)) {
+		if (direction == "up") {
 			transform.position += Vector3.up * speed * Time.deltaTime;
 		}
-		else if (Input.GetKey(KeyCode.DownArrow)) {
+		else if (direction == "down") {
 			transform.position += Vector3.down * speed * Time.deltaTime;
 		}
-		if (Input.GetKey(KeyCode.RightArrow)) {
+		if (direction == "right") {
 			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
-		else if (Input.GetKey(KeyCode.LeftArrow)) {
+		else if (direction == "left") {
 			transform.position += Vector3.left * speed * Time.deltaTime;
 		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.gameObject.layer == 8){
-			Destroy(other.gameObject);
+		if (other.gameObject.layer == 8) {
+			Destroy (other.gameObject);
 		}
 	}
 }
