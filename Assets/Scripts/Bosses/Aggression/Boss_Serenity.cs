a6a69af59184e7c8ac70787158c5c4110b383af3
Jono <jono@defectivestudios.com>
2016-01-30 17:14:58 -0500

    Brough Roh's level back from the dead; start of Serenity scene.

---
+++ Assets/Scripts/Bosses/Aggression/Boss_Serenity.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Boss_Serenity : MonoBehaviour {
+
+	public static Boss_Serenity instance;
+
+	public float surviveTime = 15f;
+
+	public static float RemainingTime { get { return instance.surviveTime - Time.timeSinceLevelLoad; } }
+
+	void Start() {
+		instance = this;
+	}
+}
