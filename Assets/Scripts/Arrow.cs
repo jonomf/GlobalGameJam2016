0b36a7420216ed7eda0e75e38c2320ee1ab6a113
Sam Shapiro <samuel.shapiro123@gmail.com>
2016-01-30 09:54:01 -0500

    pulled arrow shooting, added preliminary character sprite

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class Arrow : MonoBehaviour {
 
-<<<<<<< HEAD
 	string direction = "";
 	float speed = 20;
 
@@ -27,14 +26,8 @@ public class Arrow : MonoBehaviour {
 			transform.position += Vector3.left * speed * Time.deltaTime;
 		}
 	}
-
-	void OnTriggerEnter2D(Collider2D other) {
-		if (other.gameObject.layer == 8) {
-			Destroy (other.gameObject);
-		}
-=======
+		
 	void OnCollisionEnter2D(Collision2D coll) {
 		Destroy(gameObject);
->>>>>>> 7fe4f0821c3eb25173cde8bdd262429d8090031d
 	}
 }
