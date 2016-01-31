11f40f6fddd9cf5c2b45d874e211b72a65d85a1b
Sam Shapiro <samuel.shapiro123@gmail.com>
2016-01-30 19:38:40 -0500

    picking up a relic now heals you 10, not 50

--- Assets/Scripts/Relic.cs
+++ Assets/Scripts/Relic.cs
@@ -5,7 +5,7 @@ public class Relic : MonoBehaviour {
 
 	void OnCollisionEnter2D (Collision2D other) {
 		if (other.gameObject.layer == Layers.PlayerNum) {
-			Player.GetHurt (-50);
+			Player.GetHurt (-10);
 			Boss_Greed.Regenerate ();
 		} else {
 			Destroy (gameObject);
