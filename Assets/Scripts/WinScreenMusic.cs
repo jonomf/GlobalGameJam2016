8461ea6e151fc299bfb7d37646f6319900d6efed
Jono <jono@defectivestudios.com>
2016-01-31 14:54:58 -0500

    Added graphics for altar, pillar, coin; implemented music for win, lose, and transition.

---
+++ Assets/Scripts/WinScreenMusic.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinScreenMusic : MonoBehaviour {
+
+	public AudioSource fanfare;
+	public AudioSource winTheme;
+
+	void Awake() {
+		StartCoroutine(PlayWinThemeAfterFanfare());
+	}
+
+	private IEnumerator PlayWinThemeAfterFanfare() {
+		yield return new WaitForSeconds(fanfare.clip.length);
+		winTheme.Play();
+	}
+}
