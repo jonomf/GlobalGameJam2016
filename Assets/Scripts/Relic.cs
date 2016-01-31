5513b8e98ca134b415dc535462af7043be3c12ca
Sam Shapiro <samuel.shapiro123@gmail.com>
2016-01-30 19:36:34 -0500

    added preliminary greed boss fight

---
+++ Assets/Scripts/Relic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Relic : MonoBehaviour {
+
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.layer == Layers.PlayerNum) {
+			Player.GetHurt (-50);
+			Boss_Greed.Regenerate ();
+		} else {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/UI/UIController_GreedBoss.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIController_GreedBoss : MonoBehaviour {
+
+	public static UIController_GreedBoss instance;
+
+	public Slider bossHealthSlider;
+
+	void Awake() {
+		instance = this;
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public static void UpdateBossHealth () {
+		instance.bossHealthSlider.value = Boss_Greed.Health;
+	}
+}
