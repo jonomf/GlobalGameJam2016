e7e041c83aba56c8e240e513cdaae00230e1a19f
Rohit Crasta <rohitcrasta@gmail.com>
2016-01-31 11:50:46 -0500

    New Scenes: Start->HowTo->HowTo2->Game

---
+++ Assets/Scripts/UI/UIController_HowTo_1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController_HowTo_1 : MonoBehaviour {
+    public static UIController_HowTo_1 instance;
+    private InputDevice inputDevice;
+
+    void Awake(){
+        instance = this;
+    }
+
+    void Update() {
+        //Check for A button to change scene to correct boss
+        inputDevice = InputManager.ActiveDevice;
+        if(Input.GetKeyDown(KeyCode.A) || inputDevice.GetControl(InputControlType.Action1).WasPressed){
+            SceneManager.LoadScene("HowToPlay_2");
+        }
+    }
+}
+
