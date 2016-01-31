7664cc4474be30b8cdeb33db0592e969b752e83b
Rohit Crasta <rohitcrasta@gmail.com>
2016-01-31 13:58:07 -0500

    Added Global Timer to game scene

--- Assets/Scripts/UI/UIController_GameScene.cs
+++ Assets/Scripts/UI/UIController_GameScene.cs
@@ -6,11 +6,22 @@ public class UIController_GameScene : MonoBehaviour {
 
 	public static UIController_GameScene instance;
 
-	public Slider boss1Slider;
+	public Text secondsLeftText;
+    public Slider boss1Slider;
 	public Slider boss2Slider;
 	public Slider boss3Slider;
+
+    public float numSeconds = 30;
+    public static float RemainingGameTime { get { return instance.numSeconds - Time.timeSinceLevelLoad; } }
 
 	void Awake() {
 		instance = this;
 	}
+
+    void Update() {
+        secondsLeftText.text = string.Format("{0:00} seconds left!", RemainingGameTime);
+        if (RemainingGameTime <= 0){
+            GameController.Lose();
+        }
+    }
 }
