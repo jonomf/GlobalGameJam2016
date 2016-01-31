6b228c613f5dce54f3e56feaf76392075c2eb951
Rohit Crasta <rohitcrasta@gmail.com>
2016-01-31 13:17:58 -0500

    Player can loop back to the game from Win/Lose

---
+++ Assets/Scripts/UI/UIController_Win.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController_Win : MonoBehaviour {
+    public static UIController_Win instance;
+    private InputDevice inputDevice;
+
+    void Awake(){
+        instance = this;
+    }
+
+    void Update() {
+        //Check for A button to change scene to correct boss
+        inputDevice = InputManager.ActiveDevice;
+        if(Input.GetKeyDown(KeyCode.A) || inputDevice.GetControl(InputControlType.Action1).WasPressed){
+            SceneManager.LoadScene("Game scene");
+        }
+    }
+}
