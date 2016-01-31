c34f745256afe5ffac6a851b3e85cf924b53b473
Jono <jono@defectivestudios.com>
2016-01-30 20:11:08 -0500

    Serenity boss drops walls and backs away fast, and you have an arrow that points at the boss.

---
+++ Assets/Scripts/Bosses/Serenity/BossDirectionIndicator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossDirectionIndicator : MonoBehaviour {
+
+	void Update() {
+		Quaternion rotation = Quaternion.LookRotation(Boss_Serenity.Position - transform.position, transform.TransformDirection(Vector3.up));
+		transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+	}
+}
