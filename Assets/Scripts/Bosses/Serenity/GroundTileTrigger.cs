1c733b8d53b51c5a5b5fbbb6889ac84b60ff08d6
Jono <jono@defectivestudios.com>
2016-01-30 23:36:31 -0500

    Infinite ground for Serenity.

---
+++ Assets/Scripts/Bosses/Serenity/GroundTileTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundTileTrigger : MonoBehaviour {
+
+	public Vector2 direction;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.layer == Layers.PlayerNum){
+			Instantiate(transform.parent.gameObject,
+				(Vector2) transform.position + direction * (transform.parent.GetComponent<Collider>().bounds.size.x / 2),
+				Quaternion.identity);
+			Destroy(gameObject);
+		}
+	}
+}
